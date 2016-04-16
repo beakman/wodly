0893388106f8af32db028a67b42044b2372d34d7
Paco <psalido@gmail.com>
2016-04-16 09:35:28 +0200

    Add Wod to UserSerializer

--- wodly/users/serializers.py
+++ wodly/users/serializers.py
@@ -2,6 +2,8 @@ from .models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+	wods = serializers.StringRelatedField(many=True)
+
 	class Meta:
 		model = User
-		fields = ('username', 'email')
+		fields = ('username', 'email', 'wods')
